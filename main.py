c372e1262b68f6e4698ef921216f8bcc691ba92d
rosg89 <rsuarezgalan@hotmail.com>
2022-05-20 11:09:16 -0300

    Se sube el proyecto

---
+++ main.py
@@ -0,0 +1,22 @@
+#While Loop
+numero = 0
+
+while numero <10:
+  print(numero)
+  numero +=1
+#Imprime del 0 al 9
+
+  
+#For Loop
+list_of_items = [1,2,3]
+
+for item in list_of_items:
+  print(item)
+#Imprime 1, 2, 3
+
+  
+#For range
+for number in range(1,10):
+  print(number)
+#Imprime del 1 al 9
+
